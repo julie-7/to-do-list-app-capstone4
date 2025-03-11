164f3004419ee8631d14bb891abaf49765b56620
frlopez88 <frlopez88@icloud.com>
2025-03-10 19:21:37 -0600

    Corrections

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import {user} from './routes/userroutes.js';
-import {task} from  './routes/taskroutes.js';
+import { user } from './routes/userroutes.js';
+import { task } from './routes/taskroutes.js';
+import jwt from 'jsonwebtoken'
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users',user);
-app.use('/api/tasks',task);
+app.use('/api/users', user);
 
-    const tokenValidation = (req,res,next) => {
+const tokenValidation = (req, res, next) => {
     const authorization = req.headers['authorization'];
-    if(!authorization){
-        return res.status(400).json({message:"you need to pass a token"});
+    if (!authorization) {
+        return res.status(400).json({ message: "you need to pass a token" });
     }
-    const token= authorization.replace('Bearer','').trim();
-try{
-    const secret= process.env.KEY_SECRET;
-    Jwt.verify(token,secret);
-    next();
-}catch(err) {
-    return res.status(400).json({message:"Invalid Token"});
-}
+    const token = authorization.replace('Bearer', '').trim();
+    try {
+        const secret = process.env.KEY_SECRET;
+        jwt.verify(token, secret);
+        next();
+    } catch (err) {
+        return res.status(400).json({ message: "Invalid Token" });
+    }
 
 };
-app.use('api/task',tokenValidation,task);
+app.use('/api/tasks', tokenValidation, task);
 const PORT = process.env.PORT;
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
